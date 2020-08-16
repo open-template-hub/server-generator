0247c11a4e16616ef9b0f2b5efd23ecf139dfa2b
fatihturker <fatih.turker@optiva.com>
2020-08-16 16:21:29 +0300

    Adding empty line to EOF

--- src/constant.ts
+++ src/constant.ts
@@ -2,7 +2,7 @@ export const TemplateType = {
   PaymentServer: "1",
   AuthServer: "2",
   BasicInfoServer: "3",
-  FileStorageServer: "4"
+  FileStorageServer: "4",
 }
 
 export const TEMPLATE_HUB_URL = "https://github.com/open-template-hub";
@@ -13,12 +13,12 @@ export const ProjectName = {
   PaymentServer: "payment-server-template",
   AuthServer: "auth-server-nodejs-template",
   BasicInfoServer: "basic-info-server-template",
-  FileStorageServer: "file-storage-server-template"
+  FileStorageServer: "file-storage-server-template",
 }
 
 export const PackageName = {
   PaymentServer: "payment-server",
   AuthServer: "auth-server-nodejs-template",
   BasicInfoServer: "basic-info-server",
-  FileStorageServer: "file-storage-server-template"
+  FileStorageServer: "file-storage-server-template",
 }
