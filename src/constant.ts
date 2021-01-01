d2936b9cb8c7ff5fdca15994187230eabd89388f
fatihturker <fatih.turker@optiva.com>
2021-01-01 22:44:55 +0300

    Add Analytics Server

--- src/constant.ts
+++ src/constant.ts
@@ -1,24 +1,27 @@
 export const TemplateType = {
-  PaymentServer: "1",
-  AuthServer: "2",
-  BasicInfoServer: "3",
-  FileStorageServer: "4",
-}
+  PaymentServer: '1',
+  AuthServer: '2',
+  BasicInfoServer: '3',
+  FileStorageServer: '4',
+  AnalyticsServer: '5',
+};
 
-export const TEMPLATE_HUB_URL = "https://github.com/open-template-hub";
+export const TEMPLATE_HUB_URL = 'https://github.com/open-template-hub';
 
-export const BRANCH_NAME = "master";
+export const BRANCH_NAME = 'master';
 
 export const ProjectName = {
-  PaymentServer: "payment-server-template",
-  AuthServer: "auth-server-nodejs-template",
-  BasicInfoServer: "basic-info-server-template",
-  FileStorageServer: "file-storage-server-template",
-}
+  PaymentServer: 'payment-server-template',
+  AuthServer: 'auth-server-nodejs-template',
+  BasicInfoServer: 'basic-info-server-template',
+  FileStorageServer: 'file-storage-server-template',
+  AnalyticsServer: 'analytics-server-template',
+};
 
 export const PackageName = {
-  PaymentServer: "payment-server",
-  AuthServer: "auth-server-nodejs-template",
-  BasicInfoServer: "basic-info-server",
-  FileStorageServer: "file-storage-server-template",
-}
+  PaymentServer: 'payment-server',
+  AuthServer: 'auth-server-nodejs-template',
+  BasicInfoServer: 'basic-info-server',
+  FileStorageServer: 'file-storage-server-template',
+  AnalyticsServer: 'analytics-server-template',
+};
--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,39 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as shell from 'shelljs';
 import chalk from 'chalk';
-import * as yargs from 'yargs';
+import yargs from 'yargs';
 import { ncp } from 'ncp';
-import * as rmdir from 'rimraf';
-import { TemplateType, BRANCH_NAME, ProjectName, TEMPLATE_HUB_URL, PackageName } from './constant';
+import rmdir from 'rimraf';
+import {
+  TemplateType,
+  BRANCH_NAME,
+  ProjectName,
+  TEMPLATE_HUB_URL,
+  PackageName,
+} from './constant';
 
 /**
  * @description holds the project generator
  */
 
- // Questions
+// Questions
 const QUESTIONS = [
   {
     name: 'template',
     type: 'input',
-    message: '1) Payment Server \n'
-           + '  2) Auth Server \n'
-           + '  3) Basic Info Server \n'
-           + '  4) File Storage Server \n'
-           +'  Please enter a server type you want to generate: ',
+    message:
+      '1) Payment Server \n' +
+      '  2) Auth Server \n' +
+      '  3) Basic Info Server \n' +
+      '  4) File Storage Server \n' +
+      '  5) Analytics Server \n' +
+      '  Please enter a server type you want to generate: ',
     when: () => !yargs.argv['template'],
     validate: (input: string) => {
-      if (/^[1 | 2 | 3 | 4]$/.test(input)) return true;
-      else return 'Please enter correct server type. Server type can be 1, 2, 3 and 4.';
-    }
+      if (/^[1 | 2 | 3 | 4 | 5]$/.test(input)) return true;
+      else
+        return 'Please enter correct server type. Server type can be 1, 2, 3, 4 and 5.';
+    },
   },
   {
     name: 'name',
@@ -37,135 +46,175 @@ const QUESTIONS = [
     when: () => !yargs.argv['template'],
     validate: (input: string) => {
       if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
-    }
-  }
+      else
+        return 'Project name may only include letters, numbers, underscores and hashes.';
+    },
+  },
 ];
 
 // current directory
 const CURR_DIR = process.cwd();
 
 // prompts questions to user
-inquirer.prompt(QUESTIONS)
-  .then(answers => {
+inquirer.prompt(QUESTIONS).then((answers) => {
+  let userAnswers = Object.assign({}, answers, yargs.argv);
 
-    let userAnswers = Object.assign({}, answers, yargs.argv);
+  const projectName = userAnswers['name'];
+  const targetPath = path.join(CURR_DIR, projectName);
 
-    const projectName = userAnswers['name'];
-    const targetPath = path.join(CURR_DIR, projectName);
+  if (!createProject(targetPath)) {
+    return;
+  }
 
-    if (!createProject(targetPath)) {
-      return;
+  const templateType = userAnswers['template'];
+
+  if (!cloneTemplate(targetPath, templateType)) {
+    console.log(chalk.red('Can not clone the selected template.'));
+    return;
+  }
+
+  if (!updateProjectName(targetPath, templateType, projectName)) {
+    console.log(chalk.red('Can not set the project name.'));
+    return;
+  }
+
+  if (!postProcessNode(targetPath)) {
+    return;
+  }
+
+  showMessage(projectName);
+});
+
+const updateProjectName = (
+  targetPath: string,
+  templateType: string,
+  projectName: string
+) => {
+  let existingProjectName = '';
+  let existingPackageName = '';
+  shell.cd(targetPath);
+
+  switch (templateType) {
+    case TemplateType.AuthServer:
+      existingProjectName = ProjectName.AuthServer;
+      existingPackageName = PackageName.AuthServer;
+      break;
+    case TemplateType.PaymentServer:
+      existingProjectName = ProjectName.PaymentServer;
+      existingPackageName = PackageName.PaymentServer;
+      break;
+    case TemplateType.BasicInfoServer:
+      existingProjectName = ProjectName.BasicInfoServer;
+      existingPackageName = PackageName.BasicInfoServer;
+      break;
+    case TemplateType.FileStorageServer:
+      existingProjectName = ProjectName.FileStorageServer;
+      existingPackageName = PackageName.FileStorageServer;
+      break;
+    case TemplateType.AnalyticsServer:
+      existingProjectName = ProjectName.AnalyticsServer;
+      existingPackageName = PackageName.AnalyticsServer;
+      break;
+  }
+
+  let oldPath = path.join(targetPath, existingProjectName);
+
+  ncp(oldPath, targetPath, function (err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      rmdir(oldPath, (errRmDir: any) => {
+        if (errRmDir) {
+          return console.log(errRmDir);
+        } else {
+          let packageFile = path.join(targetPath, 'package.json');
+
+          fs.readFile(packageFile, 'utf8', function (errReadFile, data) {
+            if (errReadFile) {
+              return console.log(errReadFile);
+            }
+            var result = data.replace(existingPackageName, projectName);
+
+            fs.writeFile(packageFile, result, 'utf8', function (errWriteFile) {
+              if (errWriteFile) return console.log(errWriteFile);
+            });
+          });
+          const gitFolderPath = path.join(targetPath, '.git');
+          rmdir(gitFolderPath, (errRmDirInner: any) => {
+            if (errRmDirInner) {
+              return console.log(errRmDirInner);
+            }
+          });
+        }
+      });
     }
-
-    const templateType = userAnswers['template'];
-
-    if(!cloneTemplate(targetPath, templateType)) {
-      console.log(chalk.red('Can not clone the selected template.'));
-      return;
-    }
-
-    if(!updateProjectName(targetPath, templateType, projectName)) {
-      console.log(chalk.red('Can not set the project name.'));
-      return;
-    }
-
-    if (!postProcessNode(targetPath)) {
-      return;
-    }
-
-    showMessage(projectName);
   });
 
-  const updateProjectName = (targetPath: string, templateType: string, projectName: string) => {
-    let existingProjectName = "";
-    let existingPackageName = "";
-    shell.cd(targetPath);
+  return true;
+};
 
-    switch(templateType) {
-      case TemplateType.AuthServer:
-        existingProjectName = ProjectName.AuthServer;
-        existingPackageName = PackageName.AuthServer;
-        break;
-      case TemplateType.PaymentServer:
-        existingProjectName = ProjectName.PaymentServer;
-        existingPackageName = PackageName.PaymentServer;
-        break;
-      case TemplateType.BasicInfoServer:
-        existingProjectName = ProjectName.BasicInfoServer;
-        existingPackageName = PackageName.BasicInfoServer;
-        break;
-      case TemplateType.FileStorageServer:
-        existingProjectName = ProjectName.FileStorageServer;
-        existingPackageName = PackageName.FileStorageServer;
-        break;
-    }
+const cloneTemplate = (targetPath: string, templateType: string) => {
+  shell.cd(targetPath);
 
-    let oldPath = path.join(targetPath, existingProjectName);
+  let cmd = '';
+  let clone = 'git clone -b ';
 
-    ncp(oldPath, targetPath, function (err) {
-      if (err) {
-        return console.log(err);
-      } else {
-        rmdir(oldPath, (errRmDir: any) => {
-          if (errRmDir) {
-            return console.log(errRmDir);
-          } else {
-            let packageFile = path.join(targetPath, 'package.json');
+  switch (templateType) {
+    case TemplateType.AuthServer:
+      cmd =
+        clone +
+        BRANCH_NAME +
+        ' ' +
+        TEMPLATE_HUB_URL +
+        '/' +
+        ProjectName.AuthServer;
+      break;
+    case TemplateType.PaymentServer:
+      cmd =
+        clone +
+        BRANCH_NAME +
+        ' ' +
+        TEMPLATE_HUB_URL +
+        '/' +
+        ProjectName.PaymentServer;
+      break;
+    case TemplateType.BasicInfoServer:
+      cmd =
+        clone +
+        BRANCH_NAME +
+        ' ' +
+        TEMPLATE_HUB_URL +
+        '/' +
+        ProjectName.BasicInfoServer;
+      break;
+    case TemplateType.FileStorageServer:
+      cmd =
+        clone +
+        BRANCH_NAME +
+        ' ' +
+        TEMPLATE_HUB_URL +
+        '/' +
+        ProjectName.FileStorageServer;
+      break;
+    case TemplateType.AnalyticsServer:
+      cmd =
+        clone +
+        BRANCH_NAME +
+        ' ' +
+        TEMPLATE_HUB_URL +
+        '/' +
+        ProjectName.AnalyticsServer;
+      break;
+  }
+  console.log('command: ', cmd);
+  const result = shell.exec(cmd);
 
-            fs.readFile(packageFile, 'utf8', function (errReadFile,data) {
-              if (errReadFile) {
-                return console.log(errReadFile);
-              }
-              var result = data.replace(existingPackageName, projectName);
-
-              fs.writeFile(packageFile, result, 'utf8', function (errWriteFile) {
-                 if (errWriteFile) return console.log(errWriteFile);
-              });
-            });
-            const gitFolderPath = path.join(targetPath, ".git");
-            rmdir(gitFolderPath, (errRmDirInner: any) => {
-              if (errRmDirInner) {
-                return console.log(errRmDirInner);
-              }
-            });
-          }
-      });
-      }
-    });
-
-    return true;
+  if (result.code !== 0) {
+    return false;
   }
 
-  const cloneTemplate = (targetPath: string, templateType: string) => {
-    shell.cd(targetPath);
-
-    let cmd = "";
-    let clone = "git clone -b ";
-
-    switch(templateType) {
-      case TemplateType.AuthServer:
-        cmd = clone + BRANCH_NAME + " " + TEMPLATE_HUB_URL + "/" + ProjectName.AuthServer;
-        break;
-      case TemplateType.PaymentServer:
-        cmd = clone + BRANCH_NAME + " " + TEMPLATE_HUB_URL + "/" + ProjectName.PaymentServer;
-        break;
-      case TemplateType.BasicInfoServer:
-        cmd = clone + BRANCH_NAME + " " + TEMPLATE_HUB_URL + "/" + ProjectName.BasicInfoServer;
-        break;
-      case TemplateType.FileStorageServer:
-        cmd = clone + BRANCH_NAME + " " + TEMPLATE_HUB_URL + "/" + ProjectName.FileStorageServer;
-        break;
-    }
-    console.log("command: ", cmd);
-    const result = shell.exec(cmd);
-
-    if (result.code !== 0) {
-      return false;
-    }
-
-    return true;
-  }
+  return true;
+};
 
 /**
  * shows message to user
@@ -175,7 +224,7 @@ const showMessage = (projectName: string) => {
   console.log('');
   console.log(chalk.green('Done.'));
   console.log(chalk.green(`Go into the project: cd ${projectName}`));
-}
+};
 
 /**
  * creates project
@@ -184,13 +233,15 @@ const showMessage = (projectName: string) => {
  */
 const createProject = (projectPath: string) => {
   if (fs.existsSync(projectPath)) {
-    console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
+    console.log(
+      chalk.red(`Folder ${projectPath} exists. Delete or use another name.`)
+    );
     return false;
   }
 
   fs.mkdirSync(projectPath);
   return true;
-}
+};
 
 /**
  * applies post process for node,
@@ -219,4 +270,4 @@ const postProcessNode = (targetPath: string) => {
   }
 
   return true;
-}
+};
