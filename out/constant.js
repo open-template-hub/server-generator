358d48806d5334fef67d8b6c3789fffbc62cf297
fatihturker <fatih.turker@optiva.com>
2021-01-01 23:03:35 +0300

    Fix commands

---
+++ out/constant.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PackageName = exports.ProjectName = exports.BRANCH_NAME = exports.TEMPLATE_HUB_URL = exports.TemplateType = void 0;
+exports.TemplateType = {
+    PaymentServer: '1',
+    AuthServer: '2',
+    BasicInfoServer: '3',
+    FileStorageServer: '4',
+    AnalyticsServer: '5',
+};
+exports.TEMPLATE_HUB_URL = 'https://github.com/open-template-hub';
+exports.BRANCH_NAME = 'master';
+exports.ProjectName = {
+    PaymentServer: 'payment-server-template',
+    AuthServer: 'auth-server-nodejs-template',
+    BasicInfoServer: 'basic-info-server-template',
+    FileStorageServer: 'file-storage-server-template',
+    AnalyticsServer: 'analytics-server-template',
+};
+exports.PackageName = {
+    PaymentServer: 'payment-server',
+    AuthServer: 'auth-server-nodejs-template',
+    BasicInfoServer: 'basic-info-server',
+    FileStorageServer: 'file-storage-server-template',
+    AnalyticsServer: 'analytics-server-template',
+};
+//# sourceMappingURL=constant.js.map
