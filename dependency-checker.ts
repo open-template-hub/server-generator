695315a13f039c11a3ea2d738289c11b2a3dcea3
Furkan Yavuz <furknyavuz@gmail.com>
2022-06-16 14:21:43 +0300

    update dependency checker

--- dependency-checker.ts
+++ dependency-checker.ts
@@ -5,16 +5,18 @@ const lines = outdatedCmd.stdout.toString().split( '\n' );
 
 const columnIndexes = [ 0, 0, 0, 0 ];
 
+let indexOfDependedBy = -1;
+
 console.log(
     '<p align="center">\n' +
     '  <a href="https://opentemplatehub.com">\n' +
-    '    <img src="https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/logo/generator/server-generator-logo.png" alt="Logo" width=200>\n' +
+    '    <img src="https://raw.githubusercontent.com/open-template-hub/open-template-hub.github.io/master/assets/logo/ui/web-ui-logo.png" alt="Logo" width=200>\n' +
     '  </a>\n' +
     '</p>\n' +
     '\n' +
     '\n' +
     '<h1 align="center">\n' +
-    'Open Template Hub - Server Generator v4\n' +
+    'Open Template Hub - Company Profile UI Template v4\n' +
     '  <br/>\n' +
     '(outdated packages)\n' +
     '</h1>\n' +
@@ -26,36 +28,46 @@ for ( const line of lines ) {
   if ( line.length === 0 ) {
     continue;
   }
-  
+
   if ( lines.indexOf( line ) === 0 ) {
     columnIndexes[ 0 ] = line.indexOf( 'Current' );
     columnIndexes[ 1 ] = line.indexOf( 'Wanted' ) + 3;
     columnIndexes[ 2 ] = line.indexOf( 'Latest' ) + 6;
     columnIndexes[ 3 ] = line.indexOf( 'Location' ) + 9;
   }
-  
+
   let modifiedLine = '';
-  
+
   if ( columnIndexes [ 0 ] >= 0 ) {
     const stringParts = line.split( /(\s+)/ );
-    
+
     modifiedLine += '| ';
-    
-    for ( let part of stringParts ) {
+
+    for ( let i = 0; i < stringParts.length; ++i ) {
+      const part = stringParts[ i ];
+
+      if ( lines.indexOf( line ) === 0 && i < stringParts.length - 1 && stringParts[ i + 1 ] === 'Depended' ) {
+        indexOfDependedBy = i;
+      }
+
+      if ( indexOfDependedBy !== -1 && i >= indexOfDependedBy ) {
+        continue;
+      }
+
       if ( part.match( /\s+/ ) ) {
         modifiedLine += ' | ';
       } else {
         modifiedLine += part;
       }
     }
-    
+
     modifiedLine += ' |';
-    
+
     console.log( modifiedLine );
   } else {
     console.log( modifiedLine );
   }
-  
+
   if ( lines.indexOf( line ) === 0 ) {
     console.log( '| --- | --- | --- | --- | --- |' );
   }
